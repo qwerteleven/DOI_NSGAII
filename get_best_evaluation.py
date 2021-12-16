0571466553c26dde688f0c7836aaec8278a6387a
qwerteleven <ll11ll1@outlook.es>
2021-12-16 19:27:28 +0000

    add get best evaluations

---
+++ get_best_evaluation.py
@@ -0,0 +1,36 @@
+import glob
+
+
+def read_stats_files(path):
+    return glob.glob(path + "/*/hyperstats.out")
+
+def get_final_value(file):
+    X = 0
+    f = open(file, "r")
+    lines = f.readlines()
+    X = float(lines[-2].split(' ')[-2])
+    f.close()
+
+    return X
+
+
+def main(path):
+    stats_files = read_stats_files(path)
+    
+    final_value = []
+
+    for stats_file in stats_files:
+        final_value.append((get_final_value(stats_file), stats_file))
+
+    final_value.sort(key=lambda x: -x[0])
+
+    print("best_values: ", [i for i in final_value[0:5]])
+
+    print("worst_values: ", [i for i in final_value[-5:-1]])
+
+    print("Done")
+
+
+if __name__ == '__main__':
+    path = './results'
+    main(path)
